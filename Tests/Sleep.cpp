da8d680226eb3f2dc1ebd09e2fdb1a99aa91344c
Tyler11223344 <ttrebuchon@live.com>
2017-05-05 15:14:03 -0400

    Update Sleep tests to use Chrono directly for timing

--- Tests/Sleep.cpp
+++ Tests/Sleep.cpp
@@ -1,6 +1,6 @@
 #include "../Tests.h"
 #include <Sleep/Sleep.h>
-#include <Stopwatch/Stopwatch.h>
+#include <chrono>
 
 #define TEST_DURATION ((long double)(0.1))
 
@@ -10,19 +10,22 @@
 
 long double TestSleep(long double time)
 {
-	Util::Stopwatch sw;
 	
-	//dout << "Testing at duration=" << time << std::endl;
-	sw.start();
+	auto start = std::chrono::high_resolution_clock::now(); //Get start time
 	Util::sleep(time, Util::Sleep::Seconds);
-	sw.stop();
-	//dout << sw.value() << std::endl;
-	long double err = sw.value() - time;
-	err*= (err < 0) ? -1 : 1;
-	//dout << "Error: " << err << std::endl;
+	auto stop = std::chrono::high_resolution_clock::now(); //Get stop time
+	auto diff = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count(); //Get delta in microseconds
+	
+	
+	long double err = ((long double)(diff - time*1000000))/1000000; //Get error in seconds
+	err*= (err < 0) ? -1 : 1; //Take abs value
 	
 	long double rel_err = (100*err)/time;
-	assert_ex((err < ACCEPTABLE_ERROR && rel_err < 100) || (rel_err < ACCEPTABLE_REL_ERROR && err < ACCEPTABLE_ERROR*10));
+	if (!((err < ACCEPTABLE_ERROR && rel_err < 100) || (rel_err < ACCEPTABLE_REL_ERROR && err < ACCEPTABLE_ERROR*10)))
+	{
+		throw StringException(std::string("(") + std::to_string(err) + " < ACCEPTABLE_ERROR(" + std::to_string(ACCEPTABLE_ERROR) + ") && " + std::to_string(rel_err) + " < 100) || (" + std::to_string(rel_err) + " < ACCEPTABLE_REL_ERROR && " + std::to_string(err) + " < ACCEPTABLE_ERROR(" + std::to_string(ACCEPTABLE_ERROR) + ") *10)");
+	}
+	//assert_ex((err < ACCEPTABLE_ERROR && rel_err < 100) || (rel_err < ACCEPTABLE_REL_ERROR && err < ACCEPTABLE_ERROR*10));
 	return rel_err;
 }
 
@@ -34,7 +37,7 @@ bool Testing::Sleep()
 	long double avg = 0;
 	avg += TestSleep(TEST_DURATION);
 	avg += TestSleep(TEST_DURATION/10);
-	avg += TestSleep(TEST_DURATION/100);
+	avg += TestSleep(TEST_DURATION*1.9);
 	dout << avg/3 << "%" << std::endl;
 	}
 	
