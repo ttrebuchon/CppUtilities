b10385635507f29740767774e36860065bdc3812
Tyler11223344 <ttrebuchon@live.com>
2017-05-08 16:35:40 -0400

    Clean up matrix tests, remove abundant output, add more targeted assertions

--- Tests/Math_Matrix.cpp
+++ Tests/Math_Matrix.cpp
@@ -10,19 +10,9 @@ using namespace Math;
 
 bool Math_Matrix()
 {
-	//dout << "\n\n\n" << std::endl;
-	
-	
-	
-	
-	
-	
-	
-	
 	FuncMatrix<1, int> c1 = FuncMatrix<1, int>([](int i) -> int { return 4*i; });
 	
 	auto s1 = c1[3];
-	dout << s1 << std::endl;
 	assert_ex(s1 == 12);
 	
 	std::function<int(int, int)> f([](int i, int j) -> int { return i*j; });
@@ -48,7 +38,6 @@ bool Math_Matrix()
 	colT1_2_1 = colT2_1[1];
 	assert_ex(colT1_2_1 == 2);
 	
-	dout << "Created..." << std::endl;
 	matTest = new FuncMatrix<2, double>([](int i, int j) { return i*(i-j); });
 	
 	int matTest_n = 15;
@@ -72,19 +61,6 @@ bool Math_Matrix()
 		dout << "\n\n" << std::endl;
 	};
 	
-	/*for (int i = 0; i < matTest.size()[0]; i++)
-	{
-		dout << "[ ";
-		for (int j = 0; j < matTest.size()[1]; j++)
-		{
-			dout << matTest[i][j] << ", ";
-		}
-		dout << "]" << std::endl;
-	}*/
-	
-	print_matrix(matTest);
-	
-	
 	
 	
 	
@@ -92,74 +68,58 @@ bool Math_Matrix()
 	auto matTestT = matTest.T();
 	assert_ex(matTestT.size()[0] == matTest.size()[1]);
 	assert_ex(matTestT.size()[1] == matTest.size()[0]);
-	dout << "\n\nTransposed: " << std::endl;
-	/*for (int i = 0; i < matTestT.size()[0]; i++)
-	{
-		dout << "[ ";
-		for (int j = 0; j < matTestT.size()[1]; j++)
-		{
-			dout << matTestT[i][j] << ", ";
-			assert_ex(matTestT[i][j] == matTest[j][i]);
-		}
-		dout << "]" << std::endl;
-	}*/
-	print_matrix(matTestT);
-	
 	
 	int removeX, removeY;
 	removeX = 1;
 	removeY = 0;
-	dout << "\n\nSubmatrix: (" << removeY << ", " << removeX << ")" << std::endl;
 	auto matTestSM = matTestT.submatrix(removeY, removeX);
-	
-	print_matrix(matTestSM);
 	
 	
 	matTest.size()[0] = matTest.size()[1] = matTestT.size()[0] = matTestT.size()[1] = 3;
 	dout << "\n\nContracting Matrices..." << std::endl;
 	
 	print_matrix(matTest);
-	//dout << "\n\n" << std::endl;
 	print_matrix(matTestT);
-	//dout << "\n\n" << std::endl;
 	
 	
 	auto matTestC = matTest.contract(matTestT);
 	dout << "\n\nContracted Matrix: " << std::endl;
 	print_matrix(matTestC);
-	//dout << "\n\n" << std::endl;
-	auto matTest_4 = matTest;
-	matTest_4.size()[0] = matTest_4.size()[1] = 4;
-	for (int i = 0; i < 1; i++)
-	{
-		matTest_4 = matTest_4.contract(matTest_4);
-	}
-	print_matrix(matTest_4);
-	//dout << "\n\n" << std::endl;
-	print_matrix(matTest_4);
+	
+	
+	
+	
+	dout << "\n\nTesting 2x2 contract 2x1..." << std::endl;
+	matrix_t<2, double, int> testC_2_2 = new FuncMatrix<2, double, int>([] (int i, int j) {
+		    return (j+1) + 2*i;
+		});
+	testC_2_2.size()[0] = testC_2_2.size()[1] = 2;
+	matrix_t<1, double, int> testC_1_2 = new FuncMatrix<1, double, int>([] (int i) {
+		    return (i+1);
+		});
+	testC_1_2.size()[0] = 2;
+	dout << testC_2_2.toString() << " • " << testC_1_2.toString() << std::endl;
+	auto testC_p = testC_2_2.contract(testC_1_2);
+	dout << testC_p.toString() << std::endl;
+	assert_ex(testC_p[0] == 5);
+	assert_ex(testC_p[1] == 11);
+	dout << "Passed\n\n" << std::endl;
+	
+	
+	
 	
 	
 	dout << "Determinant: " << std::endl;
 	print_matrix(matTestC.submatrix(0, 0));
 	dout << matTestC.submatrix(0, 0).det() << std::endl;
-	dout << matTestC.submatrix(0, 0).contract(matTestC.submatrix(0, 0)).det() << std::endl;
 	
 	auto testDets = [=] (auto m1, auto m2)
 	{
-		//dout << "\n\n" << std::endl;
 		auto m3 = m1.contract(m2);
 		
-		/*print_matrix(m1);
-		print_matrix(m2);
-		print_matrix(m3);*/
-		
 		auto d1 = m1.det();
-		//dout << "Det 1: " << d1 << std::endl;
 		auto d2 = m2.det();
-		//dout << "Det 2: " << d2 << std::endl;
 		auto d3 = m3.det();
-		//dout << "Det 3: " << d3 << std::endl;
-		
 		assert_ex(d3 == d1*d2);
 	};
 	
@@ -207,23 +167,35 @@ bool Math_Matrix()
 	});
 	consec_m_1.size()[0] = consec_size;
 	
-	/*dout << consec_m_1.toString() << std::endl;
+	auto testC_1_1 = [] (int size) -> void
+	{
+		matrix_t<2, double> m1([] (int i, int j) -> double {
+			return j+1;
+		});
+		
+		m1.size()[0] = 1;
+		m1.size()[1] = size;
+		
+		matrix_t<1, double, int> m2([] (auto i) {
+			return i+1;
+		});
+		m2.size()[0] = size;
+		
+		auto p = m1.contract(m2);
+		
+		auto ansFunc = [] (int size) -> decltype(p[0])
+		{
+			decltype(p[0]) tot = 0;
+			for (int i = 0; i < size; i++)
+			{
+				tot += (i+1)*(i+1);
+			}
+			return tot;
+		};
+		
+		assert_ex(ansFunc(size) == p[0]);
+	};
+	testC_1_1(5);
 	
-	dout << consec_m_3.contract(consec_m_1).toString() << std::endl;
-	
-	dout << "M3 Det: " << consec_m_3.det() << std::endl;
-	dout << "M2 Det: " << consec_m.det() << std::endl;
-	
-	dout << "M3 • M2 Det: " << consec_m_3.contract(consec_m).det() << std::endl;
-	dout << "M3 • M2:\n" << consec_m_3.contract(consec_m).toString() << std::endl;
-	
-	
-	
-	dout << "M2 • M1:\n" << consec_m.contract(consec_m_1).toString() << std::endl;*/
-	
-	
-
 	return true;
-	
-	
 }
