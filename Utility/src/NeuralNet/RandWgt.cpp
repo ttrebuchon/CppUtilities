42d4be063cc2a7b8d9d7dc9d480e5c89746b1afc
Tyler11223344 <ttrebuchon@live.com>
2017-05-11 17:52:21 -0400

    Modify Neural RandWgt to have range [-1, 1]

--- Utility/src/NeuralNet/RandWgt.cpp
+++ Utility/src/NeuralNet/RandWgt.cpp
@@ -18,7 +18,7 @@ namespace Internal
 		static std::default_random_engine gen;
 	};
 	
-	std::uniform_real_distribution<> _GenRandWgt::dist(0, 1);
+	std::uniform_real_distribution<> _GenRandWgt::dist(-1, 1);
 	
 	std::default_random_engine _GenRandWgt::gen;
 	
