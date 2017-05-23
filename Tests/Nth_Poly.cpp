f3b98b48d7f2031d0005cfd8f669c2cd2dd12c37
Tyler11223344 <ttrebuchon@live.com>
2017-05-23 10:35:04 -0400

    Update Nth_Poly with correct multiple regression matrix formula

--- Tests/Nth_Poly.cpp
+++ Tests/Nth_Poly.cpp
@@ -148,9 +148,7 @@ void Test_Multiple_Reg()
 	assert_ex(M.size(1) == in.size(1) + 1);
 	
 	assert_ex(M.contract(coef) == out);
-	//coef = M.T().contract(M).inv().contract(M).contract(out)
-	dout << "Before: " << M(0).toString() << " and " << M(1).toString() << std::endl;
-	M(0) += M(1);
-	dout << "After: " << M(0).toString() << std::endl;
+	//coef = M.T().contract(M).inv().contract(M.T()).contract(out)
+	assert_ex(coef == M.inv().contract(out));
 	
 }
