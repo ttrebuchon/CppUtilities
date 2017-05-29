e53f167e14b01920decdd18ebdceed291edc4f17
Tyler11223344 <ttrebuchon@live.com>
2017-05-29 18:28:57 -0400

    Change tensor_t tests to use float rather than double, to better account for rounding in larger datasets/matrices

--- Tests/tensor_t.cpp
+++ Tests/tensor_t.cpp
@@ -12,14 +12,16 @@ using namespace Math;
 assert_ex(x < TOL && x > -1*TOL); \
 dout << #x << ": " << x << std::endl
 
-double invStdDev(std::function<double(int, int)>, int h, int w);
+typedef float Num;
+
+Num invStdDev(std::function<Num(int, int)>, int h, int w);
 
 bool Testing::Tensor_T()
 {
-	tensor_t<1, double> t;
+	tensor_t<1, Num> t;
 	
 	
-	tensor_t<2, long double> t2 = new DataMatrix<2, long double>( {{1, 1}, {1, 1}});
+	tensor_t<2, Num> t2 = new DataMatrix<2, Num>( {{1, 1}, {1, 1}});
 	assert_ex(t == NULL);
 	t = 4;
 	t2(1, 1) = 4;
@@ -30,7 +32,7 @@ bool Testing::Tensor_T()
 	dout << t2.toString() << std::endl;
 	t2 = { t2[0], t2[1] };
 	dout << t2.toString() << std::endl;
-	t2 = { tensor_t<1, long double>([](int i) -> double { return i; }, 2), {2, 3}};
+	t2 = { tensor_t<1, Num>([](int i) -> decltype(t2[0][0]) { return i; }, 2), {2, 3}};
 	
 	
 	
@@ -40,7 +42,7 @@ bool Testing::Tensor_T()
 	
 	t2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-	tensor_t<1, long double> t2_P;
+	tensor_t<1, Num> t2_P;
 	auto t2_lu = LUP_Make(t2, &t2_P);
 	dout << "\n\nt2_lu: \n" << t2_lu.toString() << "\n" << std::endl;
 	dout <<  "t2_P: \n" << t2_P.toString() << "\n\n" << std::endl;
@@ -49,7 +51,7 @@ bool Testing::Tensor_T()
 	assert_ex(t2.det() == 0);
 	
 	
-	tensor_t<2, double> invTest = new FuncMatrix<2, double>([](int i, int j) {
+	tensor_t<2, Num> invTest = new FuncMatrix<2, Num>([](int i, int j) {
 		return 2*i + (j+1);
 	}, 2, 2);
 	dout << "invTest: " << invTest.toString() << std::endl;
@@ -58,7 +60,7 @@ bool Testing::Tensor_T()
 	dout << "invTest.invTesti: " << invTest.contract(invTesti).toString() << std::endl;
 	
 	int w = 2;
-	tensor_t<2, double> invT2 = new FuncMatrix<2, double>([=](int i, int j) {
+	tensor_t<2, Num> invT2 = new FuncMatrix<2, Num>([=](int i, int j) {
 		return i*w + (j+1);
 	}, 4, w);
 	
@@ -73,7 +75,7 @@ bool Testing::Tensor_T()
 	auto err = invT2 - reconstruction;
 	dout << "Err: " << err.toString() << std::endl;
 	
-	double stdDev = 0;
+	Num stdDev = 0;
 	int iSize = err.size(0);
 	int jSize = err.size(1);
 	for (int i = 0; i < iSize; i++)
@@ -89,27 +91,27 @@ bool Testing::Tensor_T()
 	
 	checkStdDev(stdDev2, invStdDev([=](int i, int j) { return i*w + (j+1); }, 4, w));
 	
-	checkStdDev(stdDev3, invStdDev([] (int i, int j) -> double { return (i == j); }, 4, 4));
+	checkStdDev(stdDev3, invStdDev([] (int i, int j) -> Num { return (i == j); }, 4, 4));
 	
-	checkStdDev(stdDev4, invStdDev([] (int i, int j) -> double { return i*2 + (j+1); }, 2, 2));
+	checkStdDev(stdDev4, invStdDev([] (int i, int j) -> Num { return i*2 + (j+1); }, 2, 2));
 	
-	checkStdDev(stdDev5, invStdDev([] (int i, int j) -> double { return sin(i+1)*cos(2*j + i); }, 2, 2));
+	checkStdDev(stdDev5, invStdDev([] (int i, int j) -> Num { return sin(i+1)*cos(2*j + i); }, 2, 2));
 	
 	return true;
 }
 
 
 
-double invStdDev(std::function<double(int, int)> f, int h, int w)
+Num invStdDev(std::function<Num(int, int)> f, int h, int w)
 {
-	tensor_t<2, double> invT2 = new FuncMatrix<2, double>(f, h, w);
+	tensor_t<2, Num> invT2 = new FuncMatrix<2, Num>(f, h, w);
 	auto invT2i = invT2.inv();
 	auto id = invT2i.contract(invT2);
 	
 	auto reconstruction = invT2.contract(id);
 	auto err = invT2 - reconstruction;
 	
-	double stdDev = 0;
+	Num stdDev = 0;
 	int iSize = err.size(0);
 	int jSize = err.size(1);
 	for (int i = 0; i < iSize; i++)
