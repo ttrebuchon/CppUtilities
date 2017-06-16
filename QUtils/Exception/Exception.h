60f1da8b8a1105178d603d40df8bdd219f53f8f2
Tyler11223344 <ttrebuchon@live.com>
2017-06-16 15:00:14 -0400

    Add Msg() method to custom exception generated classes

--- QUtils/Exception/Exception.h
+++ QUtils/Exception/Exception.h
@@ -12,10 +12,11 @@
 	std::string _what; \
 	const char* _whatC; \
 	const std::string className = #name; \
+	std::string message; \
 	\
 	public: \
 	name(std::string file, int line, std::string func) : file(file), line(line), func(func), _what(""), _whatC(NULL) \
-	{ build(); } \
+	{  } \
 	\
 	name(std::string file) : name(file, -1, "") \
 	{ } \
@@ -39,16 +40,20 @@
 	\
 	\
 	const char* what() const noexcept override { \
+	if (_whatC == NULL) \
+	{ \
+		((name*)this)->build(); \
+	} \
 	    return _whatC; \
 	}\
 	\
 	void build() { \
 	if (_what == "") \
 	{ \
-	    std::string message = #msg; \
-	    if (message == "") \
+	    _what = #msg; \
+	    if (_what == "") \
 	    { \
-	    	message = className; \
+	    	_what = className; \
 	    } \
 		std::stringstream ss; \
 		if (file != "") \
@@ -75,10 +80,22 @@
 		{ \
 			ss << "::"; \
 		} \
-		ss << message; \
+		ss << _what; \
+		if (message != "") \
+		{ \
+			ss << "::" << message; \
+		} \
 		_what = ss.str(); \
 		_whatC = _what.c_str(); \
 	} \
+	} \
+	\
+	\
+	\
+	name & Msg(std::string s) \
+	{ \
+		this->message = s; \
+		return *this; \
 	} \
 	\
 	\
