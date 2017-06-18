2860f7032bb7aa080008718a4f592c648e441901
Tyler11223344 <ttrebuchon@live.com>
2017-06-17 20:54:41 -0400

    CLIPS test work

--- Tests/CLIPS.cpp
+++ Tests/CLIPS.cpp
@@ -28,9 +28,11 @@ bool Test_CLIPS()
 		}
 	};
 
-	auto cmd = [&] (std::string command) -> void {
+	auto cmd = [&] (std::string command) -> auto {
 		dout << command << ": " << std::endl;
-		printResult(env.evaluate(command));
+		std::vector<std::string> v;
+		printResult(v = env.evaluate(command));
+		return v;
 	};
 
 	assert(env.assert_f("(mother Sue Mary)") != NULL);
@@ -131,8 +133,8 @@ bool Test_CLIPS()
 	
 	
 	
-	
-	assert(env.build("(deffacts family (father Jerry Mary) (mother Jill Mary) (father Adam Jerry) (mother Amanda Jerry) (parent Jill Sue) (mother Amanda Jack) (father Jack Mallorie))"));
+	std::string familyFacts = "(father Jerry Mary) (mother Jill Mary) (father Adam Jerry) (mother Amanda Jerry) (parent Jill Sue) (mother Amanda Jack) (father Jack Mallorie)";
+	assert(env.build("(deffacts family " + familyFacts + ")"));
 	dout << "Family defined" << std::endl;
 	assert(env.build("(defrule parent (or (logical (mother ?x ?y)) (logical (father ?x ?y))) => (assert (parent ?x ?y)))"));
 	dout << "parent defined." << std::endl;
@@ -184,9 +186,107 @@ bool Test_CLIPS()
 	cmd("(facts)");
 	
 	
+	//assert_ex(env.build("(deffunction min (?x ?y) (if (< ?x ?y) then ?x else ?y))"));
+	assert_ex(env.build("(deftemplate DoS (slot Person1) (slot Person2) (slot N))"));
+	
+	assert_ex(env.build("(deffunction NoDoS (?x ?y) (not (any-factp ((?f DoS)) (and (eq ?f:Person1 ?x) (eq ?f:Person2 ?y)))))"));
+	
+	assert_ex(env.build("(deffunction isDoS (?x ?y ?z) (not (not (any-factp ((?f DoS)) (and (eq ?f:Person1 ?x) (eq ?f:Person2 ?y) (eq ?f:N ?z))))))"));
+	
+	assert_ex(env.build("(deffunction SetDoS (?x ?y ?n) (if (not (NoDoS ?x ?y)) then (do-for-all-facts ((?f DoS)) (and (eq ?f:Person1 ?x) (eq ?f:Person2 ?y)) (modify ?f (N ?n))) else (assert (DoS (Person1 ?x) (Person2 ?y) (N ?n)))))"));
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	/*assert_ex(env.build("(defrule InitDoSParent (declare (salience -1)) (logical (parent ?x ?y)) (test (not (isDoS ?x ?y 1))) => (assert (DoS (Person1 ?x) (Person2 ?y) (N 1))))"));
+	
+	assert_ex(env.build("(defrule InitDoSSibling (declare (salience -1)) (logical (sibling ?x ?y)) (test (not (isDoS ?x ?y 1))) => (assert (DoS (Person1 ?x) (Person2 ?y) (N 1))))"));
+	
+	assert_ex(env.build("(defrule DoSRule (declare (salience -2)) (logical (DoS (Person1 ?x) (Person2 ?y) (N ?D))) (logical (DoS (Person1 ?y) (Person2 ?z) (N ?D2)))  (or (not (DoS (Person1 ?x) (Person2 ?z) (N ?D3))) (and (DoS (Person1 ?x) (Person2 ?z) (N ?D3))   (test (< (+ ?D ?D2) ?D3))   )) (test (not (eq ?x ?z)))  => "
+	
+	"(do-for-all-facts ((?f DoS)) (and (eq ?f:Person1 ?x) (eq ?f:Person2 ?z)) (retract ?f))"
+	
+	"(assert (DoS (Person1 ?x) (Person2 ?z) (N (+ ?D ?D2))))   )"));*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	assert_ex(env.build("(defrule InitDoSParent (logical (parent ?x ?y)) (test (not (isDoS ?x ?y 1))) => (SetDoS ?x ?y 1))"));
+	
+	assert_ex(env.build("(defrule InitDoSSibling (logical (sibling ?x ?y)) (test (not (isDoS ?x ?y 1))) => (SetDoS ?x ?y 1))"));
+	
+	assert_ex(env.build("(defrule DoSRule (logical (DoS (Person1 ?x) (Person2 ?y) (N ?D))) (logical (DoS (Person1 ?y) (Person2 ?z) (N ?D2))) (or (not (DoS (Person1 ?x) (Person2 ?z) (N ?D3))) (and (DoS (Person1 ?x) (Person2 ?z) (N ?D3))   (test (< (+ ?D ?D2) ?D3)) )) (test (not (eq ?x ?z)))  => "
+	
+	"(SetDoS ?x ?z (+ ?D ?D2)))"));
+	
+	
+	assert_ex(env.build("(defrule DoS_BiDir (DoS (Person1 ?y) (Person2 ?x) (N ?n)) (or (not (DoS (Person1 ?x) (Person2 ?y))) (and (DoS (Person1 ?x) (Person2 ?y) (N ?n2)) (test (< ?n ?n2)))) => (SetDoS ?x ?y ?n))"));
+	
+	
+	
+	
+	
+	cmd("(undeffacts family)");
+	dout << "\n\n\n";
+	
+	
+	std::string manyPeopleFacts = "(deffacts ManyPeople ";
+	int pCount = 10;//25;
+	int parents[pCount][2];
+	for (int i = 0; i < pCount; i++)
+	{
+		parents[i][0] = (i + 1) + (rand() % (pCount - i));
+		parents[i][1] = (i + 1) + (rand() % (pCount - i));
+	}
+	
+	
+	
+	for (int i = 0; i < pCount; i++)
+	{
+		manyPeopleFacts += "(parent P" + std::to_string(parents[i][0]) + " P" + std::to_string(i) + ") ";
+		
+		manyPeopleFacts += "(parent P" + std::to_string(parents[i][1]) + " P" + std::to_string(i) + ") ";
+	}
+	
+	manyPeopleFacts += ")";
+	assert_ex(env.build(manyPeopleFacts));
+	
+	
+	
+	env.reset();
+	env.run(-1);
+	
+	dout << "\n\n";
+	
+	assert_ex(cmd("(find-all-facts ((?f DoS) (?f2 DoS)) (and (eq ?f:Person1 ?f2:Person1) (eq ?f:Person2 ?f2:Person2) (neq ?f:N ?f2:N)))").size() == 0);
+	
+	
+	
+	
+	cmd("(undeffacts ManyPeople)");
+	assert_ex(env.build("(deffacts family " + familyFacts + ")"));
+	env.reset();
+	dout << "\n\n\n\n\n\n\n\n";
+	env.run(-1);
+	
+	cmd("(assert (parent Jones Adam))");
+	cmd("(agenda)");
+	cmd("(run)");
+	
+	
+	
+	dout << "\n\n\n\n\n\n\n";
+	cmd("(facts)");
 	
 	
 	return true;
