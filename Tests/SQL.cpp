9ff6ae2dc50b68159f67776841a248aa26b4134b
Tyler11223344 <ttrebuchon@live.com>
2017-06-19 07:41:43 -0400

    Update SQL test

--- Tests/SQL.cpp
+++ Tests/SQL.cpp
@@ -16,15 +16,27 @@ bool Test_SQL()
 	con.open();
 	assert_ex(con.isOpen());
 	
-	
+	auto dropQuery = con.tablesQuery();
+	while (dropQuery->next())
+	{
 	try
 	{
-		con.vQuery("DROP TABLE TTable;");
-	}
-	catch (...)
-	{
 		
+		auto name = dropQuery->column<std::string>(0);
+		dropQuery->reset();
+		//dout << "DROP TABLE [" << dropQuery->column<std::string>(0) << "];" << std::endl;
+		con.vQuery("DROP TABLE IF EXISTS [" + name + "];");
+		
+		//dropQuery->reset();
 	}
+	catch (std::exception& e)
+	{
+		dout << "Caught " << e.what() << std::endl;
+	}
+	}
+	delete dropQuery;
+	
+	
 	assert_ex(con.vQuery("CREATE TABLE TTable (x integer);"));
 	assert_ex(con.vQuery("INSERT INTO TTable (x) VALUES (0);"));
 	assert_ex(con.vQuery("BEGIN;"));
@@ -52,16 +64,24 @@ bool Test_SQL()
 	q->reset();
 	//(*q)();
 	dout << q->columnName(0) << ", " << q->columnName(1) << "\n";
+	bool first = true;
 	while ((*q)())
 	{
 		//dout << q->column<int>(0) << ", " << q->column<int>(1) << "\n";
+		
+		if (first)
+		{
+			first = false;
+			con.vQuery("INSERT INTO TTable (x, y) VALUES (100, 10000);");
+		}
+		
+		
 		q->next();
 	}
 	dout << q->statement() << std::endl;
 	
 	delete q;
 	q = NULL;
-	
 	
 	q = con.query("PRAGMA table_info(TTable)");
 	while ((*q)())
@@ -72,6 +92,8 @@ bool Test_SQL()
 		}
 		dout << std::endl;
 		q->next();
+		
+		
 	}
 	
 	
@@ -85,6 +107,11 @@ bool Test_SQL()
 	{
 		dout << col->name << " | " << col->type << std::endl;
 	}
+	
+	
+	assert_ex(con.vQuery("CREATE TABLE [TTable 2] (x INTEGER UNIQUE);"));
+	
+	SQL::Table TTable_2 = DB["TTable 2"];
 	
 	
 	
