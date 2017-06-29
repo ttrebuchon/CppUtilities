4a1b515f0012a861d66d01f1e6e8c889e060b81b
Tyler11223344 <ttrebuchon@live.com>
2017-06-29 09:09:26 -0400

    Symbolic list work, test work

--- QUtils/SymbolicList/SymbolicList.h
+++ QUtils/SymbolicList/SymbolicList.h
@@ -1,10 +1,91 @@
 #pragma once
+#include <vector>
+#include <functional>
+#include <memory>
 
 
 namespace QUtils
 {
 namespace SymbolicList
 {
+	template <class T>
+	class SymList
+	{
+		
+		
+		public:
+		T& operator[](const int index)
+		{
+			return at(index);
+		}
+		
+		virtual T& at(const int index) = 0;
+		
+	};
 	
+	template <class T, class Container>
+	class PointerSymList : public SymList<T>
+	{
+		private:
+		std::shared_ptr<Container> container;
+		std::function<T&(std::shared_ptr<Container>, const int)> accessor;
+		std::function<int(const std::shared_ptr<Container>)> sizer;
+		
+		
+		public:
+		template <class F, class S>
+		PointerSymList(std::shared_ptr<Container> ptr, const F f, const S s) : container(ptr), accessor([f](std::shared_ptr<Container> cont, const int index) -> T& {return f(*cont, index);}), sizer([s](const std::shared_ptr<Container> cont) -> int {return s(*cont);})
+		{
+			
+		}
+		
+		template <class F, class S>
+		PointerSymList(Container* ptr, const F f, const S s) : container(ptr), accessor([f](std::shared_ptr<Container> cont, const int index) -> T& {return f(*cont, index);}), sizer([s](const std::shared_ptr<Container> cont) -> int {return s(*cont);})
+		{
+			
+		}
+		
+		
+		virtual T& at(const int index) override
+		{
+			return accessor(container, index);
+		}
+		
+		virtual int size() const
+		{
+			return sizer(container);
+		}
+	};
+	
+	template <class T, class Iterator>
+	class IteratorSymList : public SymList<T>
+	{
+		private:
+		Iterator beginIt;
+		Iterator endIt;
+		int count;
+		std::function<T&(decltype(*std::declval<Iterator>()))> accessor;
+		public:
+		template <class F>
+		IteratorSymList(F& f) : SymList<T>(), beginIt(f.begin()), endIt(f.end()), count(0), accessor()
+		{
+			accessor = [](auto& x) -> T& { return x; };
+			
+			for (auto it = beginIt; it != endIt; it++)
+			{
+				count++;
+			}
+		}
+		
+		virtual T& at(const int index) override
+		{
+			auto it = beginIt;
+			for (int i = 0; i < index; i++)
+			{
+				it++;
+			}
+			return accessor(*it);
+		}
+	};
 }
 }
--- Tests/SymbolicList.cpp
+++ Tests/SymbolicList.cpp
@@ -2,9 +2,133 @@
 
 #include <QUtils/SymbolicList/SymbolicList.h>
 
+template <class T>
+struct Node
+{
+	T entity;
+	Node<T>* next;
+	Node<T>* prev;
+	
+	Node() : entity(), next(), prev()
+	{}
+	
+	template <class F>
+	Node(F t, Node<T>* prev, Node<T>* next) : entity(t), next(next), prev(prev)
+	{}
+	
+	Node(T t, Node<T>* prev) : entity(t), next(), prev(prev)
+	{}
+};
+
+template <class T>
+class LinkedList
+{
+	Node<T>* head;
+	Node<T>* tail;
+	unsigned int _count;
+	int currentIndex;
+	Node<T>* current;
+	
+	public:
+	LinkedList(const unsigned int size) : head(NULL), tail(NULL), _count(size), currentIndex(0), current(NULL)
+	{
+		if (size > 0)
+		{
+			std::vector<Node<T>*> nodes(size);
+			for (int i = 0; i < size; i++)
+			{
+				nodes[i] = new Node<T>();
+				if (i > 0)
+				{
+					nodes[i]->prev = nodes[i-1];
+					nodes[i-1]->next = nodes[i];
+				}
+			}
+			head = nodes.front();
+			tail = nodes.back();
+			current = head;
+		}
+	}
+	
+	
+	LinkedList() : LinkedList(0)
+	{}
+	
+	
+	int count() const
+	{
+		return _count;
+	}
+	
+	T& operator[](const int index)
+	{
+		return at(index);
+	}
+	
+	T& at(const int index)
+	{
+		while (currentIndex < index)
+		{
+			current = current->next;
+			currentIndex++;
+		}
+		while (currentIndex > index)
+		{
+			current = current->prev;
+			currentIndex--;
+		}
+		
+		return current->entity;
+	}
+	
+	template <class F>
+	T& add(F f)
+	{
+		auto n = new Node<T>(f, tail);
+		if (_count == 0)
+		{
+			head = n;
+			current = head;
+		}
+		else
+		{
+			tail->next = n;
+		}
+		tail = n;
+		_count++;
+		return tail->entity;
+	}
+	
+};
+
+using namespace QUtils;
+using namespace SymbolicList;
 
 bool Test_SymbolicList()
 {
+	auto v = new std::vector<int>(4);
+	PointerSymList<int, std::vector<int>> list(v, [](auto& c, auto index) -> int& { return c[index]; }, [](const auto& v) {return v.size();});
+	
+	v->at(1) = 2;
+	assert_ex(list[1] == 2);
+	list[0] = 1;
+	assert_ex(list[0] == 1);
+	assert_ex(v->at(0) == 1);
+	assert_ex(list.size() == 4);
+	
+	std::vector<int> v2 = {1, 2, 3, 4, 5};
+	IteratorSymList<int, std::vector<int>::iterator> list2(v2);
+	assert_ex(list2[1] == 2);
+	
+	
+	
+	
+	LinkedList<int> LL;
+	LL.add(4);
+	LL.add(6);
+	assert_ex(LL.count() == 2);
+	assert_ex(LL[0] == 4);
+	assert_ex(LL[1] == 6);
 	
 	return true;
 }
