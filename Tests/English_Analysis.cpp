d5f88e6d565cdac1f73336f00856b4540a175568
Tyler11223344 <ttrebuchon@live.com>
2017-07-08 21:54:34 -0400

    English test work

--- Tests/English_Analysis.cpp
+++ Tests/English_Analysis.cpp
@@ -1,7 +1,10 @@
 #include "../Tests_Helpers.h"
 #include <QUtils/English/Analysis.h>
 #include <QUtils/Markov/Markov.h>
-#include <QUtils/English/Analysis/Token.h>
+#include <QUtils/English/Analysis/Analysis.h>
+#include <QUtils/Stopwatch/Stopwatch.h>
+
+#include <fstream>
 
 using QUtils::English::Analyzer;
 bool Test_English_Analysis()
@@ -26,20 +29,63 @@ bool Test_English_Analysis()
 	
 	dout << "Tokens Count: " << AN.tokens.size() << "\n";
 	
+	/*QUtils::Stopwatch::Stopwatch sw;
+	sw.start();
 	
 	
 	dout << "Getting chain...\n";
-	auto MC = AN.markov();
+	auto MC = AN.markov(false);
+	sw.stop();
+	dout << "Adding data took " << sw.value() << "\n";
 	
+	sw.start();
 	dout << "Generating...\n";
 	auto chain = MC->GenerateChain();
+	sw.stop();
 	
+	std::ofstream ofile("English_Out.txt");
 	for (auto link : *chain)
 	{
 		dout << link->text();
+		ofile << link->text();
+	}
+	ofile.close();
+	dout << "------------\n";
+	dout << "Took " << sw.value() << "\n";*/
+	
+	/*for (int i = 0; i < 100; i++)
+	{
+		dout << AN.tokens[i]->text();
+	}
+	dout << "\n-----------\n";*/
+	dout << "Expanding..." << std::endl;
+	dout << "Expanded Size: " << AN.expand().size() << "\n";
+	dout << "Unique Words: " << AN.words.size() << std::endl;
+	{
+		int i = 0;
+		for (auto word : AN.words)
+		{
+			dout << word->text() << ", ";
+			if (i++ > 100)
+			{
+				dout << std::endl;
+				break;
+			}
+		}
 	}
 	
-	dout << "------------\n";
-	
+	dout << "Unique Sentences: " << AN.sentences.size() << std::endl;
+	{
+		int i = 0;
+		for (auto sen : AN.sentences)
+		{
+			dout << sen->text() << ", ";
+			if (i++ > 10)
+			{
+				dout << std::endl;
+				break;
+			}
+		}
+	}
 	return true;
 }
