be58dbf40fff76e7a5f0cb82131b216d76cf9f9f
Tyler11223344 <ttrebuchon@live.com>
2017-07-18 07:55:20 -0400

    Add start of JSON module headers

---
+++ QUtils/JSON/JSON.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace QUtils
+{
+namespace QJSON
+{
+	class JSON
+	{
+		private:
+		
+		protected:
+		
+		public:
+		
+		template <class ...T>
+		static std::string Serialize(T...);
+	};
+}
+}
