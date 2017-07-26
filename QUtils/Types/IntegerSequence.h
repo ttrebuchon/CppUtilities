a29fff488bcc6e781de08d742869fb06b62290ae
Tyler11223344 <ttrebuchon@live.com>
2017-07-26 07:35:53 -0400

    Types module - IntegerSequence.h

---
+++ QUtils/Types/IntegerSequence.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+namespace QUtils
+{
+namespace Types
+{
+	template <int...>
+	struct Sequence {};
+	
+	
+	template <int N, int... M>
+	struct SequenceGen : public SequenceGen<N-1, N, M...> 
+	{};
+	
+	template <int ...M>
+	struct SequenceGen<0, M...> : public Sequence<0, M...>
+	{
+		typedef Sequence<0, M...> type;
+	};
+	
+	
+	
+}
+}
