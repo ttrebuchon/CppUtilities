8b2260d5751272813334da3970ea66ec88bbb46d
ttrebuchon <ttrebuchon@live.com>
2017-09-23 17:45:44 -0400

    Add NULLPTRCHECK(x) macro to NullPointer.h

--- QUtils/Exception/NullPointer.h
+++ QUtils/Exception/NullPointer.h
@@ -5,3 +5,5 @@
 UTIL_CUSTOM_EXCEPTION(NullPointerException, Null pointer dereference found);
 
 #define NullPtrEx() NullPointerException("", __func__)
+
+#define NULLPTRCHECK(x) (x != NULL ? x : throw NullPointerException().Line(__LINE__).Function(__func__).File(__FILE__).Msg(std::string(#x) + " is NULL"))
