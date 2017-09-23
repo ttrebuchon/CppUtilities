fd6cbe131ffb418b5d2dc222dfa00303a9bb7de7
ttrebuchon <ttrebuchon@live.com>
2017-09-23 18:01:18 -0400

    Update "SrcTest2.cpp" CodeGen output

--- SrcTest2.cpp
+++ SrcTest2.cpp
@@ -1,4 +1,6 @@
+#define SRCTEST2
 #include <string>
+#include <vector>
 namespace TestNS
 {
 	namespace TestNS2
@@ -11,5 +13,10 @@ namespace TestNS
 		{
 			return "Hello!\b,\nworld.\b!\tTest\vTest2\n\n\fTest3\a\a\a\n\\Testing!";
 		}
+		double numFoo()
+		{
+			return 1998777.000000;
+		}
+		 std::vector<int> x = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	}
 }
