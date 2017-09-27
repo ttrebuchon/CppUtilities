3e1163677299b95fd43c7975b271096af6ea0441
ttrebuchon <ttrebuchon@live.com>
2017-09-27 09:34:43 -0400

    Add descriptive exception and additional methods to Networking::ProcedureLookup

--- QUtils/Network/Service/ProcedureLookup.h
+++ QUtils/Network/Service/ProcedureLookup.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <map>
+#include <vector>
+
+#include <QUtils/Exception/Exception.h>
 
 
 namespace QUtils
 {
 namespace Network
 {
+	QUTILS_CUSTOM_EXCEPTION(ProcedureNotFound, the remote procedure could not be found);
+	
 	template <class Index_t, class Service_t, class Return_t, class...Args>
 	class ProcedureLookup
 	{
@@ -23,11 +28,19 @@ namespace Network
 		public:
 		static Procedure Find(const Index_t index)
 		{
+			if (procedures.count(index) <= 0)
+			{
+				throw ProcedureNotFound().Function(__func__).Line(__LINE__).File(__FILE__).Msg("with index \"" + index + "\"");
+			}
 			return procedures.at(index);
 		}
 		
 		static Procedure_c Find_c(const Index_t index)
 		{
+			if (procedures_c.count(index) <= 0)
+			{
+				throw ProcedureNotFound().Function(__func__).Line(__LINE__).File(__FILE__).Msg("with index \"" + index + "\"");
+			}
 			return procedures_c.at(index);
 		}
 		
@@ -39,6 +52,38 @@ namespace Network
 		static void Register(const Index_t index, Procedure_c proc)
 		{
 			procedures_c[index] = proc;
+		}
+		
+		static auto Count(const Index_t index)
+		{
+			return procedures.count(index);
+		}
+		
+		static auto Count_c(const Index_t index)
+		{
+			return procedures_c.count(index);
+		}
+		
+		static std::vector<Index_t> Keys()
+		{
+			std::vector<Index_t> vec(procedures.size());
+			int ind = 0;
+			for (auto pair : procedures)
+			{
+				vec[ind++] = pair.first;
+			}
+			return vec;
+		}
+		
+		static std::vector<Index_t> Keys_c()
+		{
+			std::vector<Index_t> vec(procedures_c.size());
+			int ind = 0;
+			for (auto pair : procedures_c)
+			{
+				vec[ind++] = pair.first;
+			}
+			return vec;
 		}
 	};
 	
