485d9c84dcd10f3bf0dd9cb693ef3ff22a637c49
Tyler11223344 <ttrebuchon@live.com>
2017-10-01 13:59:24 -0400

    Remove static qualifier from ABC_RESULT in ASM.cpp test

--- Tests/ASM.cpp
+++ Tests/ASM.cpp
@@ -5,8 +5,7 @@
 
 extern "C"
 {
-    static int _ABC_RESULT = 24;
-    const int& ABC_RESULT = _ABC_RESULT;
+    volatile int ABC_RESULT = 24;
     int abc123456()
     {
         return ABC_RESULT;
@@ -53,6 +52,12 @@ static_assert(false, "Unknown architecture!");
 
 DEF_TEST(ASM)
 {
+    if (!(asmFunc() == (abc123456()+10)) || !(asmFunc() == ABC_RESULT+10) || !(abc123456() == ABC_RESULT))
+    {
+        dout << "asmFunc() == " << asmFunc() << std::endl;
+        dout << "abc123456() == " << abc123456() << std::endl;
+        dout << "ABC_RESULT == " << ABC_RESULT << std::endl;
+    }
     assert_ex(asmFunc() == (abc123456()+10));
     assert_ex(asmFunc() == ABC_RESULT+10);
     assert_ex(abc123456() == ABC_RESULT);
