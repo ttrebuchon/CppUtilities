68b84b7cec13cf0d20c06f9dacb22de5099cb46e
ttrebuchon <ttrebuchon@live.com>
2017-10-07 17:16:31 -0400

    Add Network\All.h

--- QUtils/Network/Service.h
+++ QUtils/Network/Service.h
@@ -1,10 +1,3 @@
 #pragma once
 
-#include "Service/Service.h"
-#include "Service/Router.h"
-#include "Service/Message.h"
-#include "Service/ServiceFactory.h"
-#include "Service/Client.h"
-#include "Service/LocalClient.h"
-#include "Service/ReturnMessage.h"
-#include "Service/RPCMessage.h"
+#include "Service/All.h"
---
+++ QUtils/Network/Service/All.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+#include "Router.h"
+#include "Message.h"
+#include "Channel.h"
+#include "SendChannel.h"
+#include "Client.h"
+#include "LocalClient.h"
+#include "ServiceFactory.h"
+#include "Service.h"
+
+#include "JsonMessage.h"
+#include "ReturnMessage.h"
+#include "RPCMessage.h"
+
+#include "LocalChannel.h"
+#include "SocketChannel.h"
