b5648138344b0ef9bfda43e0df50c0fb5e076384
ttrebuchon <ttrebuchon@live.com>
2017-10-07 13:35:21 -0400

    Network SocketService test work

--- Tests/Network_SocketService.cpp
+++ Tests/Network_SocketService.cpp
@@ -1,7 +1,9 @@
 #include "Tests_Helpers.h"
 
 #include <QUtils/Network/Service/Service.h>
+#include <QUtils/Network/Service/Channel.h>
 
+void Test_Channel();
 void Test_TimeSrv();
 
 DEF_TEST(Network_SocketService)
@@ -11,6 +13,41 @@ DEF_TEST(Network_SocketService)
 	return true;
 }
 
+void Test_Channel()
+{
+	using namespace QUtils::Network;
+	class TestChannel : public Channel
+	{
+		public:
+		TestChannel() : Channel()
+		{
+			
+		}
+		
+		auto Test_getNext()
+		{
+			return this->getNext();
+		}
+		
+		auto Test_getAll()
+		{
+			return this->getAll();
+		}
+		
+		
+		
+		void Test_addMessage(std::shared_ptr<Message> msg)
+		{
+			this->addMessage(msg);
+		}
+	};
+	
+	
+	auto chan1 = std::make_shared<TestChannel>();
+	assert_ex(!chan1->hasMessages);
+	chan1->Test_addMessage(NULL);
+	assert_ex(!chan1->hasMessages);
+}
 
 void Test_TimeSrv()
 {
