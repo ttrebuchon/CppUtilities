ecb0aeac8d966e038fad1f899bf6c5e439d87065
ttrebuchon <ttrebuchon@live.com>
2017-10-15 15:09:32 -0400

    Commit missing files

---
+++ QUtils/Graph/DependencyGraph.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+namespace QUtils { namespace Graph {
+	
+	
+	template <class = void, class = unsigned long long>
+	class DependencyGraph;
+	
+	template <class ID_t>
+	class DependencyGraph<void, ID_t>
+	{
+		private:
+		
+		protected:
+		
+		
+		public:
+	};
+	
+	
+	template <class T, class ID_t>
+	class DependencyGraph : public DependencyGraph<void, ID_t>
+	{
+		private:
+		
+		protected:
+		
+		
+		public:
+	};
+}
+}
---
+++ Tests/Graph_DependencyGraph.cpp
@@ -0,0 +1,8 @@
+#include "../Tests_Helpers.h"
+
+
+DEF_TEST(Graph_DependencyGraph)
+{
+	return true;
+	
+}
