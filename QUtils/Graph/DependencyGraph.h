44693c2aa734d53a480cc9523db561e43df9aa1f
ttrebuchon <ttrebuchon@live.com>
2017-10-18 10:36:11 -0400

    DependencyGraph work

--- QUtils/Graph/DependencyGraph.h
+++ QUtils/Graph/DependencyGraph.h
@@ -1,33 +1,49 @@
 #pragma once
 
+#include "DependencyGraph/Node.h"
+#include <atomic>
+#include <type_traits>
+#include <deque>
+#include <QUtils/Types/TypeSequence.h>
+
 
 namespace QUtils { namespace Graph {
 	
 	
-	template <class = void, class = unsigned long long>
-	class DependencyGraph;
 	
-	template <class ID_t>
-	class DependencyGraph<void, ID_t>
+	template <class T = void>
+	class DependencyGraph
 	{
+		public:
+		typedef typename DependencyGraph_NS::Node<T> Node_t;
+		typedef typename Node_t::Priority_t Priority_t;
+		
+		
 		private:
+		Priority_t nextPriority;
 		
 		protected:
-		
-		
-		public:
-	};
-	
-	
-	template <class T, class ID_t>
-	class DependencyGraph : public DependencyGraph<void, ID_t>
-	{
-		private:
-		
-		protected:
-		
+		std::deque<std::shared_ptr<Node_t>> roots;
 		
 		public:
+		DependencyGraph() : roots()
+		{
+			
+		}
+		
+		template <class... F>
+		std::enable_if_t<std::is_same<typename QUtils::Types::FirstTypeOrVoid<F...>::type, T>::value, std::shared_ptr<Node_t>> push_back(F... f)
+		{
+			static_assert(sizeof...(F) < 2, "insert() only takes one argument");
+			
+			auto node = std::make_shared<Node_t>(nextPriority++, f...);
+			
+			//TODO
+			
+			return node;
+		}
+		
+		
 	};
 }
 }
