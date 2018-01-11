1e853a94d508b328dbb10f9a152c52cabfe1b11b
ttrebuchon <ttrebuchon@live.com>
2018-01-11 13:49:39 -0500

    Make Events\EventHandler thread-safe

--- QUtils/Events/EventHandler.h
+++ QUtils/Events/EventHandler.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <functional>
 #include <set>
+#include <mutex>
+#include <shared_mutex>
 
 namespace QUtils {
 	
@@ -8,6 +10,7 @@ namespace QUtils {
 	class EventHandler
 	{
 		protected:
+		mutable std::shared_timed_mutex handlers_m;
 		std::vector<std::function<void(Args...)>> handlers;
 		
 		public:
@@ -15,17 +18,20 @@ namespace QUtils {
 		template <class T>
 		EventHandler& operator+=(const std::function<T(Args...)> handler)
 		{
+			std::lock_guard<std::shared_timed_mutex> lock(handlers_m);
 			return *this += std::function<void(Args...)>(handler);
 		}
 		
 		EventHandler& operator+=(const std::function<void(Args...)> handler)
 		{
+			std::lock_guard<std::shared_timed_mutex> lock(handlers_m);
 			handlers.push_back(handler);
 			return *this;
 		}
 		
 		virtual void operator()(Args... args) const
 		{
+			std::shared_lock<std::shared_timed_mutex> lock(handlers_m);
 			for (const auto& hand : handlers)
 			{
 				hand(args...);
@@ -34,6 +40,7 @@ namespace QUtils {
 		
 		void clear()
 		{
+			std::lock_guard<std::shared_timed_mutex> lock(handlers_m);
 			handlers.clear();
 		}
 		
--- QUtils/Events/EventHandler_NoExcept.h
+++ QUtils/Events/EventHandler_NoExcept.h
@@ -10,6 +10,7 @@ namespace QUtils {
 		public:
 		virtual void operator()(Args... args) const
 		{
+			std::shared_lock<std::shared_timed_mutex> lock(this->handlers_m);
 			for (auto& handler : this->handlers)
 			{
 				try
