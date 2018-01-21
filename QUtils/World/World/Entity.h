b2dd0ccf3063423f11320f6dc4675a6568510863
ttrebuchon <ttrebuchon@live.com>
2018-01-20 19:17:31 -0500

    Flesh out World\Entity some more

--- QUtils/World/World/Entity.h
+++ QUtils/World/World/Entity.h
@@ -8,14 +8,24 @@ namespace QUtils { namespace World {
 	
 	class Entity : public WorldObject
 	{
+		public:
+		WORLD_OBJECT(Entity)
+		
 		private:
 		
 		protected:
+		Entity(World_t*, const Vec_t);
 		Entity(World_t*);
+		Vec_t _pos;
 		
 		public:
-		WORLD_OBJECT(Entity)
 		EffectSet effects;
+		
+		inline Vec_t pos() const
+		{ return _pos; }
+		
+		inline void setPos(const Vec_t p)
+		{ _pos = p; }
 		
 		virtual ~Entity();
 		
--- src/World/World/Entity.cpp
+++ src/World/World/Entity.cpp
@@ -1,8 +1,14 @@
 #include <QUtils/World/World/Entity.h>
+#include <iostream>
 
 namespace QUtils { namespace World {
 	
-	Entity::Entity(World_t* world) : WorldObject(world), effects()
+	Entity::Entity(World_t* world, const Vec_t pos) : WorldObject(world), _pos(pos), effects()
+	{
+		
+	}
+	
+	Entity::Entity(World_t* world) : Entity(world, Vec_t(0,0,0))
 	{
 		
 	}
