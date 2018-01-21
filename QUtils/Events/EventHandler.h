40917f444243be5f603fd1a6a2c253642eec3336
ttrebuchon <ttrebuchon@live.com>
2018-01-20 19:06:09 -0500

    EventHandler work

--- QUtils/Events/EventHandler.h
+++ QUtils/Events/EventHandler.h
@@ -3,6 +3,7 @@
 #include <set>
 #include <mutex>
 #include <shared_mutex>
+#include <list>
 
 namespace QUtils {
 	
@@ -11,7 +12,7 @@ namespace QUtils {
 	{
 		protected:
 		mutable std::shared_timed_mutex handlers_m;
-		std::vector<std::function<void(Args...)>> handlers;
+		std::list<std::function<void(Args...)>> handlers;
 		
 		public:
 		
@@ -26,6 +27,13 @@ namespace QUtils {
 		{
 			std::lock_guard<std::shared_timed_mutex> lock(handlers_m);
 			handlers.push_back(handler);
+			return *this;
+		}
+		
+		EventHandler& operator+=(const std::function<void()> handler)
+		{
+			std::lock_guard<std::shared_timed_mutex> lock(handlers_m);
+			handlers.push_back([=](auto... args) { handler(); });
 			return *this;
 		}
 		
@@ -44,5 +52,56 @@ namespace QUtils {
 			handlers.clear();
 		}
 		
+		size_t size() const
+		{
+			return handlers.size();
+		}
+	};
+	
+	
+	
+	
+	template <>
+	class EventHandler<>
+	{
+		protected:
+		mutable std::shared_timed_mutex handlers_m;
+		std::list<std::function<void()>> handlers;
+		
+		public:
+		
+		template <class T>
+		EventHandler& operator+=(const std::function<T()> handler)
+		{
+			std::lock_guard<std::shared_timed_mutex> lock(handlers_m);
+			return *this += std::function<void()>(handler);
+		}
+		
+		EventHandler& operator+=(const std::function<void()> handler)
+		{
+			std::lock_guard<std::shared_timed_mutex> lock(handlers_m);
+			handlers.push_back(handler);
+			return *this;
+		}
+		
+		virtual void operator()() const
+		{
+			std::shared_lock<std::shared_timed_mutex> lock(handlers_m);
+			for (const auto& hand : handlers)
+			{
+				hand();
+			}
+		}
+		
+		void clear()
+		{
+			std::lock_guard<std::shared_timed_mutex> lock(handlers_m);
+			handlers.clear();
+		}
+		
+		size_t size() const
+		{
+			return handlers.size();
+		}
 	};
 }
