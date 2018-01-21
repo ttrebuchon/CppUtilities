a2150460477c2dd28f9e6630a1b0edb376e33ca0
ttrebuchon <ttrebuchon@live.com>
2018-01-20 19:09:32 -0500

    Rework Effect class

--- QUtils/World/World/Effect.h
+++ QUtils/World/World/Effect.h
@@ -1,20 +1,31 @@
 #pragma once
-#include "SharedPtrOnly.h"
+#include "WorldObject.h"
 #include "Time.h"
+#include <QUtils/Events.h>
 
 namespace QUtils { namespace World {
 	
-	class Effect
+	class Entity;
+	
+	class Effect : public WorldObject
 	{
 		protected:
+		Effect(World_t*);
 		
+		EventHandler<std::shared_ptr<Entity>, bool&> effect;
 		
 		public:
-		SHARED_PTR_ONLY(Effect)
+		WORLD_OBJECT(Effect)
+		EventHandler_Noexcept<> applying;
+		EventHandler_Noexcept<> applied;
 		
 		virtual ~Effect();
 		
 		virtual Timespan time() const = 0;
+		
+		void apply(std::shared_ptr<Entity>, bool& remove);
+		
+		
 	};
 }
 }
--- src/World/World/Effect.cpp
+++ src/World/World/Effect.cpp
@@ -1,10 +1,23 @@
 #include <QUtils/World/World/Effect.h>
+#include <QUtils/World/World/Entity.h>
 
 namespace QUtils { namespace World {
+	
+	Effect::Effect(World_t* world) : WorldObject(world), effect(), applying(), applied()
+	{
+		
+	}
 	
 	Effect::~Effect()
 	{
 		
+	}
+	
+	void Effect::apply(Entity::Ptr ent, bool& remove)
+	{
+		applying();
+		effect(ent, remove);
+		applied();
 	}
 }
 }
