7a757c351e4457edde246660fae18da9fd5901e8
ttrebuchon <ttrebuchon@live.com>
2018-01-20 19:13:17 -0500

    Specifically track Instances in World_t, flesh out run-loop some more

--- QUtils/World/World/World.h
+++ QUtils/World/World/World.h
@@ -12,6 +12,7 @@
 namespace QUtils { namespace World {
 	
 	class WorldObject;
+	class Instance;
 	
 	class World_t
 	{
@@ -19,12 +20,20 @@ namespace QUtils { namespace World {
 		
 		
 		protected:
-		std::shared_timed_mutex objects_m;
+		
+		typedef std::shared_timed_mutex Shared_m;
+		typedef std::lock_guard<Shared_m> FullLock_t;
+		typedef std::shared_lock<Shared_m> ReadLock_t;
+		
+		mutable Shared_m objects_m;
+		mutable Shared_m instances_m;
 		std::unordered_map<GUID, WorldObject::Ptr> objects;
 		WorldClock _clock;
+		std::vector<std::shared_ptr<Instance>> instances;
 		
 		
 		void registerObject(WorldObject*);
+		void runInstance(std::shared_ptr<Instance>);
 		
 		public:
 		typedef WorldClock::rep Tick_t;
@@ -33,6 +42,8 @@ namespace QUtils { namespace World {
 		const WorldClock& clock;
 		EventHandler_Noexcept<> paused;
 		EventHandler_Noexcept<> resumed;
+		EventHandler_Noexcept<> tickStarting;
+		EventHandler_Noexcept<WorldClock::time_point, WorldClock::time_point> tickEnding;
 		
 		World_t();
 		
--- src/World/World/World.cpp
+++ src/World/World/World.cpp
@@ -1,20 +1,35 @@
 #include <QUtils/World/World/World.h>
 #include <QUtils/World/World/WorldObject.h>
+#include <QUtils/World/World/Instance.h>
 
 #include <QUtils/Exception/NotImplemented.h>
 #include <mutex>
+#include <QUtils/Debug/dassert.h>
 
 namespace QUtils { namespace World {
 	
-	World_t::World_t() : objects_m(), objects(), _clock(), clock(_clock)
+	World_t::World_t() : objects_m(), instances_m(), objects(), _clock(), clock(_clock)
 	{
 		
 	}
 	
 	void World_t::registerObject(WorldObject* obj)
 	{
-		std::lock_guard<std::shared_timed_mutex> lock(objects_m);
-		objects[obj->id()] = obj->shared_from_this();
+		WorldObject::Ptr ptr;
+		
+		{
+			FullLock_t lock(objects_m);
+			ptr = objects[obj->id()] = obj->shared_from_this();
+		}
+		
+		dassert(ptr != nullptr);
+		
+		if (ptr->type == WorldType::Instance)
+		{
+			FullLock_t lock(instances_m);
+			instances.push_back(std::dynamic_pointer_cast<Instance>(ptr));
+			dassert(instances.back() != nullptr);
+		}
 	}
 	
 	
@@ -25,16 +40,34 @@ namespace QUtils { namespace World {
 		
 		if (length.count() < 0)
 		{
-			length = WorldClock::duration(INT_MAX);
+			length = WorldClock::max() - clock.now();
+			//length = WorldClock::duration(INT_MAX);
 		}
 		
 		try
 		{
+			std::list<std::future<void>> tasks;
+			WorldClock::time_point lastTick;
 			resumed();
 			while (clock.now() - start < length)
 			{
+				lastTick = clock.now();
+				tickStarting();
+				_clock.advance(100);
+				{
+					ReadLock_t inst_lock(instances_m);
+					for (auto inst : instances)
+					{
+						tasks.push_back(std::async(std::launch::async, &World_t::runInstance, this, inst));
+					}
+				}
 				//throw NotImp();
-				_clock.advance();
+				for (auto& task : tasks)
+				{
+					task.get();
+				}
+				tasks.clear();
+				tickEnding(lastTick, clock.now());
 			}
 		}
 		catch (...)
@@ -54,6 +87,18 @@ namespace QUtils { namespace World {
 		{
 			return this->go(length);
 		});
+	}
+	
+	void World_t::runInstance(Instance::Ptr inst)
+	{
+		bool remove;
+		for (auto ent : inst->entities)
+		{
+			for (auto effect : ent->effects)
+			{
+				effect->apply(ent, remove);
+			}
+		}
 	}
 }
 }
